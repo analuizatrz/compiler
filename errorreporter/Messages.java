11a40dcb01ddb0dd958d162aae1009d5e70bf63f
Ana Luiza Sanches <analuiza@42codelab.com>
2018-05-25 17:14:10 -0300

    correção teste 6

--- errorreporter/Messages.java
+++ errorreporter/Messages.java
@@ -1,12 +1,12 @@
 package errorreporter;
 
-import lexer.Tag;
 import java.util.Hashtable;
 import errorreporter.Messages;
 public class Messages{
 
     public Hashtable<ErrorType, String> dictionary = new Hashtable<ErrorType, String>(){
-    {
+    private static final long serialVersionUID = 1L;
+	{
         //erros lexicos
         put(ErrorType.UEOF, "Fim de arquivo inesperado");
         put(ErrorType.MFT, "Lexema mal formado");
